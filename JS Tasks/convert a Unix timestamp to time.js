854209bda24acf64a35a981bf38ff73278704011
Mohamed Aadil PS <143945971+Aadil2003@users.noreply.github.com>
2025-02-10 20:51:33 +0530

    Add files via upload

---
+++ JS Tasks/convert a Unix timestamp to time.js
@@ -0,0 +1,9 @@
+function convert(unixTimestamp) {
+  const date = new Date(unixTimestamp * 1000);
+  const hours = date.getHours().toString().padStart(2, "0");
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const seconds = date.getSeconds().toString().padStart(2, "0");
+  return `${hours}:${minutes}:${seconds}`;
+}
+
+convert(1672531200);
